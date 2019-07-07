47fc8cccb8d6435943f6ae2aa7f2ee72de01f771
logda <logda@users.github.com>
2019-07-08 00:55:26 +1000

    crawl next page loop commit

--- quotetutorial/quotetutorial/spiders/quotes_spider.py
+++ quotetutorial/quotetutorial/spiders/quotes_spider.py
@@ -19,21 +19,6 @@ class QuoteSpider(scrapy.Spider):
             items['tag'] = tag
             yield items
 
-class QuotesSpiderSpider(scrapy.Spider):
-    name = 'quotes_spider'
-    allowed_domains = ['quotes.toscrape.com']
-    start_urls = ['http://quotes.toscrape.com/']
-
-    def parse(self, response):
-        quotes = response.xpath("//div[@class='quote']")
-        for quote in quotes:
-            text = quote.xpath(
-                ".//span[@class='text']/text()").extract_first()
-            author = quote.xpath(
-                ".//small//text()").extract_first()
-            yield {'quote': text, "author": author}
-
-        next_page_url = response.xpath("//li[@class='next']//a/@href").extract_first()
-        if next_page_url:
-            absolute_next_page_url = response.urljoin(next_page_url)
-            yield scrapy.Request(absolute_next_page_url)
+        next_page = response.css("li.next a::attr(href)").get()
+        if next_page is not None:
+            yield response.follow(next_page, callback = self.parse)
